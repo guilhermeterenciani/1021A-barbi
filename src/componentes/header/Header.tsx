55c089da67734879ff69727563a1bae2ce807898
Guilherme Figueiredo Terenciani <2350676@ifms.edu.br>
2023-08-03 07:59:10 -0400

    first commit

---
+++ src/componentes/header/Header.tsx
@@ -0,0 +1,27 @@
+function Header(){
+    return(
+        <header>
+            <div>
+                <h1>Logo</h1>
+            </div>
+            <nav>
+                <ul>
+                    <li>
+                        <a href="#">Home</a>
+                    </li>
+                    <li>
+                        <a href="#">Notícias</a>
+                    </li>
+                    <li>
+                        <a href="#">Sobre</a>
+                    </li>
+                    <li>
+                        <a href="#">Contatos</a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default Header
